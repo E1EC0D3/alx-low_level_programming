11800665a821d7b8befe54cf3115601b65882508
E1EC0D3 <pepinoh0981@gmail.com>
2023-02-22 08:06:25 -0800

    Write a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 /**
-*main - Prints the last digit of random number
+*main - Program to print alphabet letter followed by new line
 *
-*Return: Return 0
+*Return: return 0
 */
 int main(void)
 {
-	int n;
-	int last;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-
-	if (last == 0)
+	int letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
+		letter = tolower(letter);
+		putchar(letter);
 	}
 	putchar('\n');
-	return(0);
-	else
-	{
-		if (last > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, last);
-		}
-		else if (last < 6)
-		{
-			printf("abcdefghijklmnopqrstuvwxyz\n", n, last);
-		}
-	}
 	return (0);
 }
